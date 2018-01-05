ffb492ef792c7588961a0cbc4ef1581caa530d7c
yannick <yannseal1@gmail.com>
2018-01-05 17:56:52 +0000

    almost done ch 5

---
+++ ch.5/determinethetype.hs
@@ -0,0 +1,5 @@
+module DetermineTheType where
+
+    -- example = head [(0 :: Integer, "doge"), (1, "kitten")]
+    -- example = if False then True else False
+    example = (length [1, 2, 3, 4, 5])
